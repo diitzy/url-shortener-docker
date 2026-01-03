27456c27ba4ab16af564b5795437b322cc36ca11
diitzy <adityaproduction011@gmail.com>
2026-01-04 06:40:41 +0700

    Add CSS styling and footer copyright

--- app/index.php
+++ app/index.php
@@ -9,12 +9,36 @@ if (isset($_POST['url'])) {
     $shortUrl = "http://localhost:8082/r/$code";
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>URL Shortener</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
 
-<h2>URL Shortener</h2>
+<div class="container">
+    <h2>URL Shortener</h2>
 
-<form method="post">
-    <input type="url" name="url" placeholder="Masukkan URL" required>
-    <button>Shorten</button>
-</form>
+    <form method="post">
+        <input type="url" name="url" placeholder="Masukkan URL" required>
+        <button>Shorten</button>
+    </form>
 
-<?php if (isset($shortUrl)) echo "<p>Short URL: <a href='$shortUrl'>$shortUrl</a></p>"; ?>
+    <?php if (isset($shortUrl)): ?>
+        <div class="result">
+            Short URL:
+            <a href="<?php echo $shortUrl; ?>" target="_blank">
+                <?php echo $shortUrl; ?>
+            </a>
+        </div>
+    <?php endif; ?>
+
+    <div class="footer">
+        © <?php echo date("Y"); ?> Muhammad Aditya Saputra (1124160009)
+    </div>
+</div>
+
+</body>
+</html>
